6352382115fc4a455050cc47e9433a53a0ffc907
Galen Rhodes <galenrhodes@me.com>
2017-06-22 15:15:21 -0400

    Initial Commit

---
+++ PGWiringX/PGWiringX.h
@@ -0,0 +1,19 @@
+//
+//  PGWiringX.h
+//  PGWiringX
+//
+//  Created by Galen Rhodes on 6/22/17.
+//  Copyright © 2017 Project Galen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PGWiringX.
+FOUNDATION_EXPORT double PGWiringXVersionNumber;
+
+//! Project version string for PGWiringX.
+FOUNDATION_EXPORT const unsigned char PGWiringXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PGWiringX/PublicHeader.h>
+
+
